09d969fc35a11a7259684febb7647d9ece31362d
Bushogun <sotto_j@hotmail.com>
2022-08-19 15:51:26 -0500

    Funcionalidad básicav1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Footer } from './Footer/Footer';
 import { ContextMenu } from './ContextMenu/ContextMenu'
 import { ShowContextMenu } from './ShowContextMenu/ShowContextMenu';
 import { BackButton } from './BackButton/BackButton'
+import { ResponseModel } from './Models/Response';
+import { postData } from './util/HttpRequest';
 import { useState, useEffect } from 'react';
 
 const dataContextMenu = [
@@ -13,16 +15,30 @@ const dataContextMenu = [
   {text: 'informacionCliente', selected: true}
 ];
 
+
 function App() {
- 
+
+  const [ informacion , setInformacion ] = useState(ResponseModel);
+
+  const consultarInfo= () => {
+    const url = "https://cors-anywhere.herokuapp.com/https://83sulg9hgc.execute-api.us-east-1.amazonaws.com/production/clientsbank";
+    const body = JSON.stringify({ a: 1, b: "Textual content" });
+    postData(url, body).then((data) => {
+      setInformacion(data);});
+      }
+
+    useEffect (()=>{
+      consultarInfo()
+    },[]);
+
+    
   return (
   <React.Fragment>
       <Header /> 
       <BackButton/>
-    <Body />
+    <Body data = {informacion} />
     <ContextMenu />
     <ShowContextMenu /> 
-
     <Footer />
     </React.Fragment>
   );
