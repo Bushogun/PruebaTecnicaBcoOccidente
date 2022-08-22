f7f1d11f61a6d876e0e32379cd8d70fbfc8906ec
Bushogun <sotto_j@hotmail.com>
2022-08-21 23:41:40 -0500

    Updates

---
+++ precache-manifest.3559548a8ecfc180745f803347335b5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56fc48c6d579f55e0731",
+    "url": "/PruebaTecnicaBcoOccidente/static/css/main.24bcef23.chunk.css"
+  },
+  {
+    "revision": "56fc48c6d579f55e0731",
+    "url": "/PruebaTecnicaBcoOccidente/static/js/main.56fc48c6.chunk.js"
+  },
+  {
+    "revision": "dcf8a51ad87f2126417e",
+    "url": "/PruebaTecnicaBcoOccidente/static/css/1.359202d1.chunk.css"
+  },
+  {
+    "revision": "dcf8a51ad87f2126417e",
+    "url": "/PruebaTecnicaBcoOccidente/static/js/1.dcf8a51a.chunk.js"
+  },
+  {
+    "revision": "07357f3cc5cc99c2c2b7",
+    "url": "/PruebaTecnicaBcoOccidente/static/js/runtime~main.07357f3c.js"
+  },
+  {
+    "revision": "698b5868b8dd2dfa32725a559ec25535",
+    "url": "/PruebaTecnicaBcoOccidente/index.html"
+  }
+];
